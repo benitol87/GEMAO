cddfec3638143af3cfd8318e307394662e3407ff
PaulMalet <paul.malet-plicque@etu.unilim.fr>
2014-12-04 17:16:54 +0100

    Maj ihm materiel

--- src/gemao/IHM/gestionMateriel/AjouterMatériel.java
+++ src/gemao/IHM/gestionMateriel/AjouterMatériel.java
@@ -8,6 +8,7 @@ import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseTrackListener;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
@@ -22,135 +23,176 @@ import org.eclipse.swt.widgets.Text;
 public class AjouterMatériel {
 	
 	
-	Shell shell = new Shell(Launcher.display, SWT.CLOSE | SWT.MIN);
-	shell.setSize(2000, 1000);
-	shell.setText("Menu Matériel");
+	private Shell shell;
+	
+	private Composite compositeTop;
+	private Composite compositeCenter;
+	private Composite compositeCO;
+	private Composite compositeCE;
+	private Composite compositeCEN;
+	private Composite compositeCES;
+	private Composite compositeBottom;
+	private Composite compositeDes;
+
+	private FillLayout fl_obs; 
+
+	private RowLayout rlCenterEst;
+	
+	private GridLayout glPrincipal;
+	private GridLayout glTop;
+	private GridLayout glCenter;
+	private GridLayout glCenterOuest;
+	private GridLayout glCenterEstNord;
+	private GridLayout glCenterEstSud;
+	private GridLayout glBottom;
 	
 	private Label l_cat;
 	private Label l_des;
-	private Label l_var_ach;
+	private Label l_val_ach;
+	private Label l_date_ach;
+	private Label l_type;
+	private Label l_qte;
+	private Label l_marque;
+	private Label l_prixU;
+	private Label l_obs;
 	
 	private Text t_des;
-	private Text t_var_ach;
+	private Text t_val_ach;
+	private Text t_date_ach;
+	private Text t_qte;
+	private Text t_marque;
+	private Text t_prixU;
+	private Text t_obs;
 	
 	private Combo c_cat;
+	private Combo c_type;
 	
+	private Button b_valider;
+	private Button b_annuler;
 	
 	public AjouterMatériel(){
+		
+		shell = new Shell(Launcher.display, SWT.CLOSE | SWT.MIN);
+		shell.setSize(2000, 1000);
+		shell.setText("Menu Matériel");
     
-			Composite compositeTop = new Composite(shell, SWT.BORDER);
-			GridLayout glTop = new GridLayout();
-			glTop.numColumns = 4;
-			compositeTop.setLayout(glTop);
+		compositeTop = new Composite(shell, SWT.BORDER);
+		glTop = new GridLayout();
+		glTop.numColumns = 4;
+		compositeTop.setLayout(glTop);
 	
-				Label l_cat = new Label(compositeTop, SWT.NONE);
-				l_cat.setText("Categorie :");
+		l_cat = new Label(compositeTop, SWT.NONE);
+		l_cat.setText("Categorie :");
     
-				final Combo c_cat = new Combo(compositeTop, SWT.NONE|SWT.READ_ONLY);
-				c_cat.add("Instrument");
-				c_cat.add("Autre");
-				c_cat.addSelectionListener(new SelectionListener() {
-
-					@Override
-					public void widgetDefaultSelected(SelectionEvent e) {
-						System.out.println(e.widget);
-						
-					}
-
-					@Override
-					public void widgetSelected(SelectionEvent e) {
-						System.out.println(((Combo)e.widget).getText().equals("Autre"));
-							
-						
-					}
-					
-				});
+		c_cat = new Combo(compositeTop, SWT.NONE|SWT.READ_ONLY);
+		c_cat.add("Instrument");
+		c_cat.add("Autre");
+				    
+		l_des = new Label(compositeTop, SWT.NONE);
+		l_des.setText("Designation :");
+	
+		t_des = new Text(compositeTop, SWT.BORDER);
+	
+	
+		l_val_ach = new Label(compositeTop, SWT.NONE);
+		l_val_ach.setText("Valeur d'achat :");
+	
+		t_val_ach = new Text(compositeTop, SWT.BORDER);
+	
+	
+		l_date_ach = new Label(compositeTop, SWT.NONE);
+		l_date_ach.setText("Date d'achat :");
     
-    
-				Label l_des = new Label(compositeTop, SWT.NONE);
-				l_des.setText("Designation :");
-	
-				Text t_des = new Text(compositeTop, SWT.BORDER);
+		t_date_ach = new Text(compositeTop, SWT.BORDER);
 	
 	
-				Label l_val_ach = new Label(compositeTop, SWT.NONE);
-				l_val_ach.setText("Valeur d'achat :");
-	
-				Text t_val_ach = new Text(compositeTop, SWT.BORDER);
-	
-	
-				Label l_date_ach = new Label(compositeTop, SWT.NONE);
-				l_date_ach.setText("Date d'achat :");
-    
-				Text t_date_ach = new Text(compositeTop, SWT.BORDER);
-	
-	
-			Composite compositeCenter = new Composite(shell, SWT.BORDER);
-			GridLayout glCenter = new GridLayout();
-			glCenter.numColumns = 3;
-			compositeCenter.setLayout(glCenter);	
+		compositeCenter = new Composite(shell, SWT.NONE);
+		glCenter = new GridLayout();
+		glCenter.numColumns = 3;
+		compositeCenter.setLayout(glCenter);	
     
     	
 	
-				Composite compositeCO = new Composite(compositeCenter, SWT.BORDER);
-				GridLayout glCenterOuest = new GridLayout();
-				glCenterOuest.numColumns = 2;
-				compositeCO.setLayout(glCenterOuest);
+		compositeCO = new Composite(compositeCenter, SWT.BORDER);
+		glCenterOuest = new GridLayout();
+		glCenterOuest.numColumns = 2;
+		compositeCO.setLayout(glCenterOuest);
 	
 	
-					Label l_type = new Label(compositeCO, SWT.NONE|SWT.READ_ONLY);
-					l_type.setText("Type :");
+		l_type = new Label(compositeCO, SWT.NONE|SWT.READ_ONLY);
+		l_type.setText("Type :");
     
-					Combo c_type = new Combo(compositeCO, SWT.NONE);
+		c_type = new Combo(compositeCO, SWT.NONE);
     
     
-					Label l_qte = new Label(compositeCO, SWT.NONE);
-					l_qte.setText("Quantité :");
+		l_qte = new Label(compositeCO, SWT.NONE);
+		l_qte.setText("Quantité :");
     
-					Text t_qte = new Text(compositeCO, SWT.BORDER);
+		t_qte = new Text(compositeCO, SWT.BORDER);
     
-					Label l_marque = new Label(compositeCO, SWT.NONE);
-					l_marque.setText("Marque :");
+		l_marque = new Label(compositeCO, SWT.NONE);
+		l_marque.setText("Marque :");
 					
-					Text t_marque = new Text(compositeCO, SWT.BORDER);
+		t_marque = new Text(compositeCO, SWT.BORDER);
     
-					Label l_prixU = new Label(compositeCO, SWT.NONE);
-					l_prixU.setText("Prix unitaire :");
+		l_prixU = new Label(compositeCO, SWT.NONE);
+		l_prixU.setText("Prix unitaire :");
     
-					Text t_prixU = new Text(compositeCO, SWT.BORDER);
+		t_prixU = new Text(compositeCO, SWT.BORDER);
 
 	
-				Composite compositeCE = new Composite(compositeCenter, SWT.BORDER);
-				GridLayout glCenterEst = new GridLayout();
-				glCenterEst.numColumns = 2;
-				compositeCE.setLayout(glCenterEst);
+		compositeCE = new Composite(compositeCenter, SWT.NONE);
+		rlCenterEst = new RowLayout(SWT.VERTICAL);
+		compositeCE.setLayout(rlCenterEst);
 	
 	
-    				Label l_obs = new Label(compositeCE, SWT.NONE);
-    				l_obs.setText("Observations :");
+    	l_obs = new Label(compositeCE, SWT.NONE);
+    	l_obs.setText("Observations :");
     
-    				Text t_obs = new Text(compositeCE, SWT.BORDER|SWT.V_SCROLL);
+    	compositeDes = new Composite(compositeCE, SWT.NONE);
+    	fl_obs = new FillLayout();
+    	compositeDes.setLayout(fl_obs);
+    				
+    	t_obs = new Text(compositeDes, SWT.BORDER|SWT.V_SCROLL);
 	
-    		Composite compositeBottom = new Composite(shell, SWT.BORDER);
-    		GridLayout glBottom = new GridLayout();
-    		glBottom.numColumns = 3;
-    		compositeBottom.setLayout(glBottom);
+    	compositeBottom = new Composite(shell, SWT.NONE);
+    	glBottom = new GridLayout();
+    	glBottom.numColumns = 3;
+    	compositeBottom.setLayout(glBottom);
     		
-    			Button b_annuler = new Button(compositeBottom, SWT.NONE);
-    			b_annuler.setText("Annuler");
+    	b_annuler = new Button(compositeBottom, SWT.NONE);
+    	b_annuler.setText("Annuler");
 	
-    			Button b_valider = new Button(compositeBottom, SWT.NONE);
-    			b_valider.setText("Valider");
+    	b_valider = new Button(compositeBottom, SWT.NONE);
+    	b_valider.setText("Valider");
 	
-    	GridLayout gridLayout = new GridLayout();
-    	shell.setLayout(gridLayout);
+    	glPrincipal = new GridLayout();
+    	shell.setLayout(glPrincipal);
     	shell.setLayout(new GridLayout());
     	
     	shell.pack();
 		shell.open();
+		this.typeEvent();
 		this.start();
 	}
     
+	public void typeEvent(){
+		c_cat.addSelectionListener(new SelectionListener() {
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				System.out.println(e.widget);
+				
+			}
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+					
+				
+			}
+			
+		});
+	}
     
     private void start() {
     	while (!shell.isDisposed()) {
