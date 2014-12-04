050ebc3b3d83b6b4c7b95bfca93f314da4cdcfb7
PaulMalet <paul.malet-plicque@etu.unilim.fr>
2014-12-04 15:17:40 +0100

    Paul test github

--- src/gemao/application/gestionMateriel/Categorie.java
+++ src/gemao/application/gestionMateriel/Categorie.java
@@ -3,6 +3,7 @@ package materiel;
 public class Categorie {
 	private final int idCategorie;
 	private final String libelleCat;
+	private string asupprimer;
 	
 	public Categorie(int idCategorie, String libelleCat) {
 		this.idCategorie = idCategorie;
