fa5ab37b055c7bf80804ef4453ea73d1c5c79da4
Masttest <r.gourier@hotmail.fr>
2014-12-04 17:21:05 +0100

    Ajout de la fonction de calcul du quotient

--- src/gemao/moduleAdherent/ctrl/CalculerQuotientCtrl.java
+++ src/gemao/moduleAdherent/ctrl/CalculerQuotientCtrl.java
@@ -1,9 +1,46 @@
 package gemao.moduleAdherent.ctrl;
 
 public class CalculerQuotientCtrl {
-	
-	public CalculerQuotientCtrl(){
-		
+
+	public CalculerQuotientCtrl() {
+
+	}
+
+	/**
+	 * Cette fonction permet de calculer
+	 * 
+	 * @param revenus
+	 *            Les revenus du foyer (doit être supérieur à 0)
+	 * @param nbPersFoyer
+	 *            Le nombre de personnes du foyer (doit être supérieur à 0)
+	 * @param nbEnfFoyer
+	 *            Le nombre d'enfants du foyer (doit être supérieur à 0)
+	 * @return Le quotient calculé
+	 */
+	public float calculerQuotient(float revenus, int nbPersFoyer, int nbEnfFoyer) {
+		float alloc, quotient = 0;
+		switch (nbEnfFoyer) {
+		case 0:
+		case 1:
+			alloc = 0;
+			break;
+		case 2:
+			alloc = 129; // getAlloc2 dans table parametre
+			break;
+		case 3:
+			alloc = 295; // getAlloc3 dans table parametre
+			break;
+		case 4:
+			alloc = 460; // getAlloc4 dans table parametre
+			break;
+		default:
+			alloc = 626; // getAlloc5 dans table parametre
+			break;
+		}
+
+		quotient = ((revenus / 12) + alloc) / nbPersFoyer;
+
+		return quotient;
 	}
 
 }
