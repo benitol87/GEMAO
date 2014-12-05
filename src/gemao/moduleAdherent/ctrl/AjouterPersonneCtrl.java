5122efa651f2577d82820395001e2fa4eea1b07c
Masttest <r.gourier@hotmail.fr>
2014-12-05 13:19:11 +0100

    AjouterPersonneCtrl

---
+++ src/gemao/moduleAdherent/ctrl/AjouterPersonneCtrl.java
@@ -0,0 +1,26 @@
+package gemao.moduleAdherent.ctrl;
+
+import gemao.entity.Personne;
+
+public class AjouterPersonneCtrl {
+	
+	public AjouterPersonneCtrl(){
+		
+	}
+
+	public void verifierInformations(Personne pers){
+		
+		/*
+		 * Controle commun à toutes les personnes
+		 * 
+		 * if(isAdherent(pers){
+		 * 		AjouterAdherentCtrl adherentCtrl = new AjouterAdherentCtrl();
+		 * 		adherentlCtrl.verifierInformations(pers);
+		 * } else {
+		 * 		AjouterPersonnelCtrl personnelCtrl = new AjouterPersonnelCtrl();
+		 * 		personnelCtrl.verifierInformations(pers);
+		 * } 
+		 */
+	}
+	
+}
