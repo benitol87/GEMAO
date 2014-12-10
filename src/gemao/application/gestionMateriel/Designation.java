067ca91dc3381906279e02af1da607a619765ebc
aleymarie <arthur.leymarie@etu.unilim.fr>
2014-12-10 09:31:10 +0100

    Ajout de Designation et DesignationDAO pour la gestion des designations de materiel (avec un test). Cheangement de l'exception lancee par la methode CategorieDAO lorsque l'id de la categorie est inferieure a 1.

---
+++ src/gemao/application/gestionMateriel/Designation.java
@@ -0,0 +1,85 @@
+package gemao.application.gestionMateriel;
+
+public class Designation {
+	private final int idDesignation;
+	private String libelleDesignation;
+
+	/**
+	 * Instantiates a new Designation.
+	 *
+	 * @param idDesignation
+	 *            l'identificateur de la designation.
+	 * @param libelleEtat
+	 *            le libelle de la designation.
+	 */
+	public Designation(int idDesignation, String libelleDesignation) {
+		this.idDesignation = idDesignation;
+		this.libelleDesignation = libelleDesignation;
+	}
+
+	/**
+	 * Retourne le libelle de la designation.
+	 * 
+	 * @return le libelle de la designation
+	 */
+	public String getLibelleDesignation() {
+		return libelleDesignation;
+	}
+
+	/**
+	 * Permet de changer le libelle de la designation.
+	 * 
+	 * @param libelleDesignation
+	 *            le nouveau libelle.
+	 */
+	public void setLibelleDesignation(String libelleDesignation) {
+		this.libelleDesignation = libelleDesignation;
+	}
+
+	/**
+	 * Permet de recuperer l'identificateur de la designation
+	 * 
+	 * @return l'identificateur de la designation.
+	 */
+	public int getIdDesignation() {
+		return idDesignation;
+	}
+
+	@Override
+	public String toString() {
+		return "Designation [idDesignation=" + idDesignation
+				+ ", libelleDesignation=" + libelleDesignation + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + idDesignation;
+		result = prime
+				* result
+				+ ((libelleDesignation == null) ? 0 : libelleDesignation
+						.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Designation other = (Designation) obj;
+		if (idDesignation != other.idDesignation)
+			return false;
+		if (libelleDesignation == null) {
+			if (other.libelleDesignation != null)
+				return false;
+		} else if (!libelleDesignation.equals(other.libelleDesignation))
+			return false;
+		return true;
+	}
+
+}
