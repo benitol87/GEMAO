506fbfa92d29538763a18b3e6594a41eb2b3abe7
Corentin Ambroise <corentin.ambroise@etu.unilim.fr>
2015-01-12 14:04:38 +0100

    Correction materiel
    Correction materiel

--- src/fr/gemao/ctrl/materiel/AjouterDesignationCtrl.java
+++ src/fr/gemao/ctrl/materiel/AjouterDesignationCtrl.java
@@ -1,5 +1,36 @@
 package fr.gemao.ctrl.materiel;
 
-public class AjouterDesignationCtrl {
+import java.sql.Connection;
 
+import fr.gemao.entity.materiel.Designation;
+
+
+public class AjouterDesignationCtrl {
+	public AjouterDesignationCtrl(){
+		
+	}
+	
+	public void ajoutDesignation(Designation designation){
+		VerifierSyntaxeDesignation verifDesignation = new verifierSyntaxeDesignation();
+		
+		if(verifDesignation.verifierInformation(designation)){
+			
+			Designation test;
+			
+			Connection co = ConnectionMySql.getInstance();
+			DesignationDAO designationDAO = new DesignationDAO(co);
+			
+			test=designationDAO.create(designation);
+			if(test == null){
+				System.out.println("Une erreur est survenue lors de l'insertion");
+			}else {
+				System.out.println("la designation à bien été ajouté");
+			}
+			
+		}else {
+			System.out
+			.println("Les informations de la designation ne sont pas valide");
+		}
+		
+	}
 }
