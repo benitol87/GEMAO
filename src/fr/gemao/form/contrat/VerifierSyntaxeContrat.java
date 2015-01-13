7df775f6930ce8c768c22ff0d1b802960da0d60e
Mr Corleone <corentinchargois@free.fr>
2015-01-13 17:36:48 +0100

    Revert "Import"
    This reverts commit 7dfa5e61a851b01e3ae0b65a866db36c51037645.

--- src/fr/gemao/form/contrat/VerifierSyntaxeContrat.java
+++ src/fr/gemao/form/contrat/VerifierSyntaxeContrat.java
@@ -55,6 +55,54 @@ public class VerifierSyntaxeContrat {
 			isValide = false;
 		}
 		
+		/**
+		 * Vérification du jour de début du contrat
+		 */
+		if (contrat.getJourDebutContrat() < 1 || contrat.getJourDebutContrat() > 31) {
+			System.out.println("Le jour de début de contrat doit être compris entre 1 et 31");
+			isValide = false;
+		}
+		
+		/**
+		 * Vérification du mois de début du contrat
+		 */
+		if (contrat.getMoisDebutContrat() < 1 || contrat.getMoisDebutContrat() > 12) {
+			System.out.println("Le mois de début de contrat doit être compris entre 1 et 12");
+			isValide = false;
+		}
+		
+		/**
+		 * Vérification de l'année de début du contrat
+		 */
+		if (contrat.getAnneeDebutContrat() < Calendar.YEAR - 100 || contrat.getAnneeDebutContrat() > Calendar.YEAR) {
+			System.out.println("L'année de début de contrat doit être compris entre " + anneeMin + " et " + anneeMax);
+			isValide = false;
+		}
+		
+		/**
+		 * Vérification du jour de fin du contrat
+		 */
+		if (contrat.getJourFinContrat() < 1 || contrat.getJourFinContrat() > 31) {
+			System.out.println("Le jour de fin de contrat doit être compris entre 1 et 31");
+			isValide = false;
+		}
+		
+		/**
+		 * Vérification du mois de fin du contrat
+		 */
+		if (contrat.getMoisFinContrat() < 1 || contrat.getMoisFinContrat() > 12) {
+			System.out.println("Le mois de fin de contrat doit être compris entre 1 et 12");
+			isValide = false;
+		}
+		
+		/**
+		 * Vérification de l'année de fin du contrat
+		 */
+		if (contrat.getAnneeFinContrat() < contrat.getAnneeDebutContrat()) {
+			System.out.println("L'année de fin de contrat ne peut pas être inférieure à l'année de début de contrat");
+			isValide = false;
+		}
+		
 		return isValide;
 	}
 }
