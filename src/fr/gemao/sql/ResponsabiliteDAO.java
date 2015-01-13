4f479050408c77b903214bb9dea2d6de90acc613
Mr Corleone <corentinchargois@free.fr>
2015-01-13 11:10:57 +0100

    Modifications sur la classe : ResponsabiliteDAO

--- src/fr/gemao/sql/ResponsabiliteDAO.java
+++ src/fr/gemao/sql/ResponsabiliteDAO.java
@@ -4,57 +4,82 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import fr.gemao.entity.Responsabilite;
 import fr.gemao.sql.exception.DAOException;
 import fr.gemao.sql.util.DAOUtilitaires;
-import fr.gemao.view.Connexion;
 
 public class ResponsabiliteDAO extends IDAO<Responsabilite> {
 	
+	/**
+	 * Constructeur de la classe ResponsabiliteDAO
+	 * @param factory
+	 */
 	public ResponsabiliteDAO(DAOFactory factory) {
 		super(factory);
 	}
 
+	/**
+	 * Redéfinition de la méthode create
+	 */
 	@Override
 	public Responsabilite create(Responsabilite obj) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Redéfinition de la méthode delete
+	 */
 	@Override
 	public void delete(Responsabilite obj) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * Redéfinition de la méthode update
+	 */
 	@Override
 	public Responsabilite update(Responsabilite obj) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Redéfinition de la méthode get
+	 */
 	@Override
 	public Responsabilite get(long id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Redéfinition de la méthode getAll
+	 */
 	@Override
 	public List<Responsabilite> getAll() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Redéinition de la méthode map
+	 */
 	@Override
 	protected Responsabilite map(ResultSet result) throws SQLException {
 		
 		return new Responsabilite(result.getInt("idResponsabilite"), result.getString("libelle"));
 	}
 	
+	/**
+	 * Méthode permettant de retourner la liste des responsabilités en fonction de l'ID d'une personne
+	 * @param idPersonne : l'ID de la personne
+	 * @return liste : la liste des reponsabilités
+	 */
 	public List<Responsabilite> getResponsabilitesParPersonne(Long idPersonne) {
 		Connection co = null;
 		PreparedStatement state = null;
