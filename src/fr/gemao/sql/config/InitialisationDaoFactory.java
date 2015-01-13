aec9369a010c8ab21aff66df7643c344772ef88f
clem19s <clemj1900@gmail.com>
2015-01-13 10:14:30 +0100

    Ajout @WebListener InitDAO

--- src/fr/gemao/sql/config/InitialisationDaoFactory.java
+++ src/fr/gemao/sql/config/InitialisationDaoFactory.java
@@ -3,10 +3,11 @@ package fr.gemao.sql.config;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
 import fr.gemao.sql.DAOFactory;
 
-
+@WebListener
 public class InitialisationDaoFactory implements ServletContextListener {
     public static final String ATT_DAO_FACTORY = "daofactory";
 
