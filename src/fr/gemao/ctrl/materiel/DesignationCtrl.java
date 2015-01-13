35fd21d0a93d63b64ed28ac28fc751f03147cef5
Corentin Ambroise <corentin.ambroise@etu.unilim.fr>
2015-01-13 09:49:40 +0100

    Ctrls
    Mise a jour ctrl Marque et designation

--- src/fr/gemao/ctrl/materiel/DesignationCtrl.java
+++ src/fr/gemao/ctrl/materiel/DesignationCtrl.java
@@ -1,10 +1,10 @@
 package fr.gemao.ctrl.materiel;
 
-import java.sql.Connection;
+import java.util.List;
 
 import fr.gemao.entity.materiel.Designation;
+import fr.gemao.sql.DAOFactory;
 import fr.gemao.sql.gestionMateriel.DesignationDAO;
-import fr.gemao.ancien_mysql.ConnectionMySql;
 
 
 
@@ -12,7 +12,14 @@ public class DesignationCtrl {
 	public DesignationCtrl(){
 		
 	}
-	
+	/**
+	 * Permet d'ajouter une designation dans la base.
+	 * 
+	 * @param idDesignation
+	 *            l'id de la designation a rajouter dans la base.
+	 * @param libelle
+	 * 			Libelle de la designation a ajouter.
+	 */
 	public void ajoutDesignation(int idDesination, String libelle){
 		if(libelle == null){
 			throw new NullPointerException("Le Libelle ne doit pas etre null");
@@ -21,6 +28,35 @@ public class DesignationCtrl {
 			throw new NullPointerException("Le Libelle ne doit pas etre vide");
 		}
 		Designation designation = new Designation(idDesination,libelle);
-		new DesignationDAO(ConnectionMySql.getInstance()).create(designation);
+		
+		new DesignationDAO(DAOFactory.getInstance()).create(designation);
+	}
+	
+	/**
+	 * Permet de supprimer une designation.
+	 * 
+	 * @param libelle
+	 *            le libelle de la designation a supprimer.
+	 */
+	public void supprimerDesignation(String libelle){
+		if (libelle == null) {
+			throw new NullPointerException(
+					"Le nom du reparateur ne doit pas etre null");
+		}
+
+		if (libelle == "") {
+			throw new IllegalArgumentException(
+					"Le nom du reparateur doit etre renseigne");
+		}
+
+		DesignationDAO desDAO = new DesignationDAO(DAOFactory.getInstance());
+
+		List<Designation> designations = desDAO.getAll();
+		for (Designation designation : designations) {
+			if (designation.getLibelleDesignation().equals(libelle)) {
+				desDAO.delete(designation);
+				break;
+			}
+		}
 	}
 }
