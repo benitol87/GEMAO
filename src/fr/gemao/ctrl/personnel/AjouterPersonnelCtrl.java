f7fc8444024f03e8e8a3b80131465ce7f534e027
DUVEAU Thomas <thomas.duveau@hotmail.fr>
2015-01-13 10:15:04 +0100

    AjouterPersonnelCtrl + RecupererPersonnelCtrl = <3

--- src/fr/gemao/ctrl/personnel/AjouterPersonnelCtrl.java
+++ src/fr/gemao/ctrl/personnel/AjouterPersonnelCtrl.java
@@ -1,66 +1,74 @@
 package fr.gemao.ctrl.personnel;
 
-import java.sql.Connection;
-
-import fr.gemao.ctrl.adherent.PersonnelDAO;
-import fr.gemao.ctrl.adherent.PersonneDAO;
 import fr.gemao.entity.Personne;
 import fr.gemao.entity.Personnel;
-import fr.gemao.entity.personnel.Personnel;
 import fr.gemao.form.personnel.VerifierSyntaxePersonnel;
+import fr.gemao.sql.DAOFactory;
+import fr.gemao.sql.PersonneDAO;
 
 /**
  * La classe AjouterPersonnelCtrl permet de contrôler l'ajout d'un personnel.
+ * 
  * @author Thomas
  *
  */
 public class AjouterPersonnelCtrl {
-	
+
 	/**
 	 * Le controleur de AjouterPersonnelCtrl
 	 */
-	public AjouterPersonnelCtrl(){
-		
+	public AjouterPersonnelCtrl() {
+
 	}
-	
+
 	/**
 	 * Méthode permettant d'ajouter un personnel dans la base de données
-	 * @param personnel : le personnel à ajouter
+	 * 
+	 * @param personnel
+	 *            : le personnel à ajouter
 	 */
 	public static void ajouterPersonnel(Personnel personnel) {
-		
+
 		if (personnel == null) {
-			throw new IllegalArgumentException("Le personnel ne peut pas être null");
+			throw new IllegalArgumentException(
+					"Le personnel ne peut pas être null");
 		}
 	}
-	
-	public static void ajouterPersonnelSyntaxe(Personnel personnel){
+
+	/**
+	 * Méthode permettant de vérifier la syntaxe du personnel.
+	 * 
+	 * @param personnel
+	 *            : Le personnel ajouté.
+	 */
+	public static void ajouterPersonnelSyntaxe(Personnel personnel) {
 		VerifierSyntaxePersonnel verifperso = new VerifierSyntaxePersonnel();
-		
+
 		if (verifperso.verifierInformations(personnel)) {
 			Personne test1;
 			Personnel test2;
-		
-		Connection co = ConnectionMySql.getInstance();
-		PersonneDAO personneDAO = new PersonneDAO(co);
-		PersonnelDAO personnelDAO = new PersonnelDAO(co);
-		
-		test1 = personneDAO.create(personnel);
-		if (test1 == null){
-			System.out.println("Une erreur est survenue lors de l'insertion");
-		} else {
-			personnel.setIdPersonne(test1.getIdPersonne());
-			test2 = personnelDAO.create(personnel);
-			if (test2 == null){
-				System.out.println("Une erreur est survenue lors de l'insertion");
+
+			DAOFactory factory = DAOFactory.getInstance();
+			PersonneDAO personneDAO = factory.getPersonneDAO();
+			PersonnelDAO personnelDAO = factory.getPersonnelDAO();
+
+			test1 = personneDAO.create(personnel);
+			if (test1 == null) {
+				System.out
+						.println("Une erreur est survenue lors de l'insertion");
 			} else {
-				System.out.println("L'adhérent à bien été ajouté");
+				personnel.setIdPersonne(test1.getIdPersonne());
+				test2 = personnelDAO.create(personnel);
+				if (test2 == null) {
+					System.out
+							.println("Une erreur est survenue lors de l'insertion");
+				} else {
+					System.out.println("L'adhérent à bien été ajouté");
+				}
 			}
-		}
-	} else {
-		System.out
-				.println("Les informations de l'adhérent ne sont pas valide");
+		} else {
+			System.out
+					.println("Les informations de l'adhérent ne sont pas valide");
 		}
 	}
 }
-
--- src/fr/gemao/ctrl/personnel/RecupererPersonnelCtrl.java
+++ src/fr/gemao/ctrl/personnel/RecupererPersonnelCtrl.java
@@ -1,5 +1,58 @@
 package fr.gemao.ctrl.personnel;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.gemao.entity.Personnel;
+import fr.gemao.entity.adherent.Adherent;
+import fr.gemao.entity.personnel.Personnel;
+import fr.gemao.sql.AdherentDAO;
+import fr.gemao.sql.DAOFactory;
+
+/**
+ * La classe personnel permet de récupérer un personnel ou de tous les lister.
+ * 
+ * @author Thomas
+ *
+ */
 public class RecupererPersonnelCtrl {
+	/**
+	 * Le constructeur de RecupererPersonnelCtrl
+	 */
+	public RecupererPersonnelCtrl() {
+
+	}
+
+	/**
+	 * Permet de récupéper un personnel en fonction de son identifiant
+	 * 
+	 * @param idPersonne
+	 *            : identifiant
+	 * @return Personnel
+	 */
+	public Personnel recupererPersonnel(Long idPersonne) {
+		DAOFactory factory = DAOFactory.getInstance();
+		PersonnelDAO personnelDAO = factory.getPersonnelDAO();
+
+		Personnel personnel = personnelDAO.get(idPersonne);
+
+		return personnel;
+	}
+
+	/**
+	 * Permet de lister le personnel.
+	 * 
+	 * @return List Liste du personnel
+	 */
+	public List<Personnel> recupererTousPersonnels() {
+		List<Personnel> listePersonnels = new ArrayList<Personnel>();
+		DAOFactory factory = DAOFactory.getInstance();
+		PersonnelDAO personnelDAO = factory.getPersonnelDAO();
+
+		listePersonnels = personnelDAO.getAll();
+
+		return listePersonnels;
+	}
 
 }
