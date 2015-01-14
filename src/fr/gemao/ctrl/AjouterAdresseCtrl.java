8209cbc66496ff96d7cb0b3d1e9372fc61f332a5
CPoimboeuf <claire.poimboeuf@etu.unilim.fr>
2015-01-14 15:19:17 +0100

    Mises à jour des contrôleurs adhérent

--- src/fr/gemao/ctrl/AjouterAdresseCtrl.java
+++ src/fr/gemao/ctrl/AjouterAdresseCtrl.java
@@ -1,7 +1,6 @@
 package fr.gemao.ctrl;
 
 import fr.gemao.entity.Adresse;
-import fr.gemao.form.VerifierSyntaxeAdresse;
 import fr.gemao.sql.AdresseDAO;
 import fr.gemao.sql.DAOFactory;
 
@@ -14,24 +13,48 @@ public class AjouterAdresseCtrl {
 	}
 	
 	/**
+	 * Méthode permettant de vérifier les informations d'une adresse
+	 * @param adresse
+	 * @return true si les informations sont valides, false sinon
+	 */
+	public boolean verifierInformations(Adresse adresse){
+		//Vérification de l'idAdresse
+				if(adresse.getIdAdresse() < 0){
+					System.out.println("L'idAdresse doit être positif...");
+					return false;
+				}
+				
+				//Vérification de l'idCommune
+				if(adresse.getIdCommune() < 0){
+					System.out.println("L'idCommune doit être positif...");
+					return false;
+				}
+				
+				//Vérification du numéro de rue
+				if(adresse.getNumRue() <= 0){
+					System.out.println("Le numéro de rue doit être strictement positif...");
+					return false;
+				}
+				
+				return true;
+	}
+	
+	/**
 	 * Méthode permettant d'ajouter une adresse dans la BD
 	 * @param adresse
 	 */
 	public void ajoutAdresse(Adresse adresse){
-		
-		VerifierSyntaxeAdresse verifAdresse = new VerifierSyntaxeAdresse();
-		
-		if(verifAdresse.verifierInformations(adresse)){
-			Adresse test;
+		if(this.verifierInformations(adresse)){
+			Adresse adr;
 			
 			DAOFactory co = DAOFactory.getInstance();
 			AdresseDAO adresseDAO = co.getAdresseDAO();
 			
-			test = adresseDAO.create(adresse);
-			if (test == null){
+			adr = adresseDAO.create(adresse);
+			if (adr == null){
 				System.out.println("Une erreur est survenue lors de l'insertion...");
 			} else {
-				adresse.setIdAdresse(test.getIdAdresse());
+				adresse.setIdAdresse(adr.getIdAdresse());
 				System.out.println("L'adresse a bien été ajoutée.");
 			}
 		}
---
+++ src/fr/gemao/ctrl/AjouterCommuneCtrl.java
@@ -0,0 +1,80 @@
+package fr.gemao.ctrl;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import fr.gemao.entity.Adresse;
+import fr.gemao.entity.Commune;
+import fr.gemao.sql.AdresseDAO;
+import fr.gemao.sql.DAOFactory;
+
+public class AjouterCommuneCtrl {
+	
+	/**
+	 * Constructeur
+	 */
+	public AjouterCommuneCtrl(){
+		
+	}
+	
+	/**
+	 * Méthode permettant de vérifier les informations d'une commune avant ajout
+	 * @param commune
+	 * @return true si les informations sont valides, false sinon
+	 */
+	public boolean verifierInformations(Commune commune){
+		String masque;
+		Pattern pattern;
+		Matcher controler;
+		
+		//Vérification de l'idCommune
+		if(commune.getIdCommune() < 0){
+			System.out.println("L'idCommune doit être positif...");
+			return false;
+		}
+		
+		//Vérification du code postal
+		if(commune.getCodePostal() <= 0){
+			System.out.println("Le code postal doit être strictement positif...");
+			return false;
+		}
+		
+		//Vérification du nom de commune
+		masque = "^[A-Za-z\\-]+$";
+		pattern = Pattern.compile(masque);
+		controler = pattern.matcher(commune.getNomCommune());
+		if (!controler.matches()) {
+			System.out.println("Le format du nom de la commune est invalide...");
+			return false;
+		}
+
+		return true;
+	}
+	
+	/**
+	 * Méthode permettant d'ajouter une commune dans la BD
+	 * @param commune
+	 */
+	public void ajoutCommune(Commune commune){
+		if(this.verifierInformations(commune)){
+			Commune com;
+			
+			DAOFactory co = DAOFactory.getInstance();
+			CommuneDAO communeDAO = co.getCommuneDAO();
+			
+			com = communeDAO.create(commune);
+			if (com == null){
+				System.out.println("Une erreur est survenue lors de l'insertion...");
+			} else {
+				commune.setIdCommune(com.getIdCommune());
+				System.out.println("La commune a bien été ajoutée.");
+			}
+		}
+		else{
+			System.out.println("Les informations de la commune ne sont pas valides...");
+		}
+		
+	}
+	}
+
+}
