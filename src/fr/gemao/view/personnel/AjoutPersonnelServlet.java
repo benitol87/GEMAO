8ea755a823926300cad616d855aa4dadb8f9acbe
DUVEAU Thomas <thomas.duveau@hotmail.fr>
2015-01-14 17:51:08 +0100

    servletPerso

--- src/fr/gemao/view/personnel/AjoutPersonnelServlet.java
+++ src/fr/gemao/view/personnel/AjoutPersonnelServlet.java
@@ -2,11 +2,20 @@
 package fr.gemao.view.personnel;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.gemao.entity.Personnel;
+import fr.gemao.entity.util.Civilite;
 
 /**
  * Servlet implementation class AjoutPersonnelServlet
@@ -28,17 +37,18 @@ public class AjoutPersonnelServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		/*
+		/**
 		 * Récupération des données saisies, envoyées en tant que paramètres de
 		 * la requète POST générée à la validation du formulaire
+		 *
 		 */
-		/*String nom = request.getParameter( "nom" );
+		String nom = request.getParameter( "nom" );
         String prenom = request.getParameter( "prenom" );
         String dateNaissance = request.getParameter( "date" );
         String telFixe = request.getParameter( "fixe" );
         String telPortable = request.getParameter( "portable" );
         String mail = request.getParameter("email");
-        String numAdresse = request.getParameter("num");
+        String adresse = request.getParameter("adresse");
         String codePostal = request.getParameter("code");
         String ville = request.getParameter("ville");
         String diplome = request.getParameter("diplome");
@@ -48,9 +58,27 @@ public class AjoutPersonnelServlet extends HttpServlet {
         String debcontrat = request.getParameter("datedeb");
         String duree = request.getParameter("duree");
         String fincontrat = request.getParameter("datefin");
-        */
         
-		/* Transmission à la page JSP en charge de l'affichage des données */
+        /**
+         * Création du personnel
+         */
+        Date dateNaiss = new Date();
+        Date dateDebCont = new Date();
+        Date dateFinCont = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/mm/yyyy");
+        try{
+        	dateNaiss = formatter.parse(dateNaissance);
+        	dateDebCont = formatter.parse(debcontrat);
+        	dateFinCont = formatter.parse(fincontrat);
+        } catch (ParseException e) {
+			e.printStackTrace();
+		}
+        List<Fonction> list = new ArrayList<>();
+        Personnel personnel = new Personnel(null, null, null, 
+        		nom, prenom, dateNaiss, telFixe, telPortable, mail, Civilite.MONSIEUR, 
+        		list, null,);
+	
+        /* Transmission à la page JSP en charge de l'affichage des données */
 		this.getServletContext()
 				.getRequestDispatcher(VUE)
 				.forward(request, response);
