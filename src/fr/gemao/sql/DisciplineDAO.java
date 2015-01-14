5bbb7964c6a3b14ed24c4a637ce45a7267e54aa9
clem19s <clemj1900@gmail.com>
2015-01-14 15:09:38 +0100

    Ajout create, get et getALL au DisciplineDAO

--- src/fr/gemao/sql/DisciplineDAO.java
+++ src/fr/gemao/sql/DisciplineDAO.java
@@ -8,8 +8,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import fr.gemao.entity.Discipline;
+import fr.gemao.entity.adherent.Adherent;
 import fr.gemao.sql.exception.DAOException;
 import fr.gemao.sql.util.DAOUtilitaires;
+import fr.gemao.sql.util.DateUtil;
 import fr.gemao.sql.util.NumberUtil;
 
 public class DisciplineDAO extends IDAO<Discipline> {
@@ -20,8 +22,33 @@ public class DisciplineDAO extends IDAO<Discipline> {
 
 	@Override
 	public Discipline create(Discipline obj) {
-		// TODO Auto-generated method stub
-		return null;
+		if (obj == null) {
+			throw new NullPointerException("La discipline ne doit pas être null");
+		}
+		Connection connexion = null;
+		PreparedStatement requete = null;
+		ResultSet result = null;
+		String sql = "INSERT INTO discipline(idDiscpline,nom) "
+				+ "VALUES (?);";
+		try {
+			connexion = factory.getConnection();
+			requete = DAOUtilitaires.initialisationRequetePreparee(connexion,
+					sql, false, obj.getIdDiscipline(), obj.getNom());
+
+			int status = requete.executeUpdate();
+
+			if (status == 0) {
+				throw new DAOException(
+						"Échec de la création de la discipline, aucune ligne ajoutée dans la table.");
+			}
+
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		} finally {
+			DAOUtilitaires.fermeturesSilencieuses(result, requete, connexion);
+		}
+
+		return this.get(obj.getIdDiscipline());
 	}
 
 	@Override
@@ -38,14 +65,56 @@ public class DisciplineDAO extends IDAO<Discipline> {
 
 	@Override
 	public Discipline get(long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Discipline discipline = null;
+		Connection connexion = null;
+		PreparedStatement requete = null;
+		ResultSet result = null;
+		String sql = "SELECT * FROM discipline WHERE idDiscipline = ?;";
+		try {
+
+			connexion = factory.getConnection();
+			requete = DAOUtilitaires.initialisationRequetePreparee(connexion,
+					sql, false, id);
+			result = requete.executeQuery();
+
+			if (result.first()) {
+				discipline = this.map(result);
+			}
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		} finally {
+			DAOUtilitaires.fermeturesSilencieuses(result, requete, connexion);
+		}
+		return discipline;
 	}
 
 	@Override
 	public List<Discipline> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Discipline> liste = new ArrayList<>();
+
+		Discipline discipline = null;
+		Connection connexion = null;
+		PreparedStatement requete = null;
+		ResultSet result = null;
+		String sql = "SELECT * FROM discipline;";
+		try {
+
+			connexion = factory.getConnection();
+			requete = DAOUtilitaires.initialisationRequetePreparee(connexion,
+					sql, false);
+			result = requete.executeQuery();
+
+			while (result.next()) {
+				discipline = this.map(result);
+				liste.add(discipline);
+			}
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		} finally {
+			DAOUtilitaires.fermeturesSilencieuses(result, requete, connexion);
+		}
+
+		return liste;
 	}
 	
 	public List<Discipline> getDisciplineParAdherent(long idAdherent){
