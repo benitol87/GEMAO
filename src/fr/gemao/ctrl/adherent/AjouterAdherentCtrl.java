927024b21f29c92bca99897ec33661af3498780b
CPoimboeuf <claire.poimboeuf@etu.unilim.fr>
2015-01-15 09:40:52 +0100

    Modifications sur les contrôleurs

--- src/fr/gemao/ctrl/adherent/AjouterAdherentCtrl.java
+++ src/fr/gemao/ctrl/adherent/AjouterAdherentCtrl.java
@@ -1,5 +1,7 @@
 package fr.gemao.ctrl.adherent;
 
+import java.util.Date;
+
 import fr.gemao.ctrl.AjouterPersonneCtrl;
 import fr.gemao.entity.adherent.Adherent;
 import fr.gemao.sql.AdherentDAO;
@@ -14,34 +16,84 @@ public class AjouterAdherentCtrl {
 
 	}
 	
+	/**
+	 * Méthode permettant de vérifier la validité des informations d'un adhérent
+	 * @param adherent
+	 * @return true si les informations sont valides, false sinon
+	 */
 	public boolean verifierInformations(Adherent adherent){
-		//A implémenter
+		Date date = new Date();
+		
+		//Vérification de l'idMotif
+		if(adherent.getIdMotif() == null || adherent.getIdMotif() <= 0){
+			System.out.println("L'idMotif est invalide...");
+			return false;
+		}
+		
+		//Vérification de l'idResponsable
+		if(adherent.getIdResponsable() != null && adherent.getIdResponsable() <= 0){
+			return false;
+		}
+		
+		//Vérification de la date d'entrée
+		if(adherent.getDateEntree() == null || adherent.getDateEntree().after(date)){
+			System.out.println("La date d'entrée doit être antérieure à la date actuelle...");
+			return false;
+		}
+		
+		//Vérification de la date de sortie
+		if(adherent.getDateSortie() != null && adherent.getDateSortie().after(date)){
+			System.out.println("La date de sortie doit être antérieure à la date actuelle...");
+			return false;
+		}
+		
+		//Vérification de la cotisation
+		if(adherent.getCotisation() == null){
+			System.out.println("La cotisation doit être spécifiée...");
+			return false;
+		}
+		
+		//Vérification de la/des disciplines
+		if(adherent.getDisciplines().isEmpty()){
+			System.out.println("L'adhérent doit être inscrit à au moins une discipline...");
+			return false;
+		}
+		
 		return true;
 	}
 
 	/**
-	 * Méthode permettant d'ajouter un adhérent dans la BD
+	 * Méthode permettant d'ajouter un adhérent dans la BD.
+	 * Pour être ajouté, la méthode vérifie que les informations de l'adhérent sont valides et que l'ajout de la personne a été réalisé.
 	 * @param adherent
 	 */
 	public void ajoutAdherent(Adherent adherent) {
 
 		if (this.verifierInformations(adherent)) {
 			AjouterPersonneCtrl ajoutPers = new AjouterPersonneCtrl();
+			
+			//Ajout de la personne dans la base
 			if(ajoutPers.ajoutPersonne(adherent) != -1){
 				Adherent adh;
 
 				DAOFactory co = DAOFactory.getInstance();
 				AdherentDAO adherentDAO = co.getAdherentDAO();
 
-				adh = adherentDAO.create(adherent);
-				if (adh == null){
-					System.out.println("Une erreur est survenue lors de l'insertion");
-				} else {
-					System.out.println("L'adhérent à bien été ajouté");
+				//Vérification de la validité des informations
+				if(this.verifierInformations(adherent)){
+					adh = adherentDAO.create(adherent);
+					if (adh == null){
+						System.out.println("Une erreur est survenue lors de l'insertion...");
+					} else {
+						System.out.println("L'adhérent a bien été ajouté.");
+					}
+				}
+				else{
+					System.out.println("Les informations de l'adhérent ne sont pas valides...");
 				}
 			}
 			else{
-				System.out.println("Une erreur est survenue lors de l'insertion");
+				System.out.println("Une erreur est survenue lors de l'insertion...");
 			}
 
 		} else {
