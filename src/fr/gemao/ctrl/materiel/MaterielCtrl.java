df694c35dd79c93c1abbf7efb6ca8f937ffe7372
Mr Corleone <corentinchargois@free.fr>
2015-01-15 09:49:47 +0100

    Modifs MaterielCtrl

--- src/fr/gemao/ctrl/materiel/MaterielCtrl.java
+++ src/fr/gemao/ctrl/materiel/MaterielCtrl.java
@@ -12,29 +12,53 @@ import fr.gemao.entity.materiel.Materiel;
 import fr.gemao.sql.DAOFactory;
 import fr.gemao.sql.materiel.MaterielDAO;
 
+/**
+ * Contrôleur de la classe Materiel
+ * @author Coco
+ *
+ */
 public class MaterielCtrl {
+	
+	/**
+	 * Constructeur
+	 */
 	public MaterielCtrl() {
 
 	}
 
-	public void ajoutMateriel(Etat etat,Categorie categorie, Marque marque, Designation designation,String typeMat,long numSerie,Date dateAchat, float valeurAchat,float valeurReap, boolean deplacable,String observation) {
-		if(etat == null){
+	/**
+	 * Permet d'ajouter un matériel
+	 * @param etat : l'état du matériel
+	 * @param categorie : la catégorie
+	 * @param marque : la marque
+	 * @param designation : la désignation
+	 * @param typeMat : le type de matériel
+	 * @param numSerie : le numéro de série
+	 * @param dateAchat : la date d'achat
+	 * @param valeurAchat : la valeur d'achat
+	 * @param valeurReap : la valeur de ré-approvisionnement
+	 * @param deplacable : si le matériel est déplaçable ou non
+	 * @param observation : l'observation
+	 * @param quantite : la quantité
+	 */
+	public void ajoutMateriel(Etat etat, Categorie categorie, Marque marque, Designation designation, String typeMat, long numSerie, Date dateAchat, float valeurAchat, float valeurReap, boolean deplacable, String observation, int quantite) {
+		if (etat == null) {
 			throw new NullPointerException("L'etat ne doit pas etre null");
 		}
 		
-		if(categorie == null){
+		if (categorie == null) {
 			throw new NullPointerException("La categorie ne doit pas etre null");
 		}
 		
-		if(marque == null){
+		if (marque == null) {
 			throw new NullPointerException("la marque ne doit pas etre null");
 		}
 		
-		if(designation == null){
+		if (designation == null) {
 			throw new NullPointerException("La designation ne doit pas etre null");
 		}
 		
-		if(typeMat == null){
+		if (typeMat == null) {
 			throw new NullPointerException("le type de materiel ne doit pas etre null");
 		}
 		
@@ -42,35 +66,38 @@ public class MaterielCtrl {
 			throw new IllegalArgumentException("le type doit etre rempli");
 		}
 		
-		if(dateAchat == null){
+		if (dateAchat == null) {
 			throw new NullPointerException("La date d'achat ne doit pas etre null");
 		}
 		
-		if(valeurAchat < 0.0){
+		if (valeurAchat < 0.0) {
 			throw new IllegalArgumentException("la valeur d'achat ne peut pas etre negative");
 		}
 		
-		if(valeurReap < 0.0){
+		if (valeurReap < 0.0) {
 			throw new IllegalArgumentException("la valeur ne peut pas etre negative");
 		}
 		
-		Materiel materiel = new Materiel(0, etat, categorie, marque, designation, observation, numSerie, dateAchat, valeurReap, valeurReap, deplacable, observation);
+		if (quantite < 0) {
+			throw new IllegalArgumentException("La quantité ne peut pas être négative");
+		}
 		
-		new MaterielDAO(DAOFactory.getInstance()).create(materiel);
+		Materiel materiel = new Materiel(null, etat, categorie, marque, designation, typeMat, numSerie, dateAchat, valeurAchat, valeurReap, deplacable, observation, quantite);
 		
-		
+		new MaterielDAO(DAOFactory.getInstance()).create(materiel);		
 	}
 	
-	public Materiel recupererMateriel(int idMateriel){
-		if(idMateriel <= 0 ){
+	public Materiel recupererMateriel(int idMateriel) {
+		if (idMateriel <= 0 ) {
 			throw new IllegalArgumentException("Id incorrect");
 		}
+		
 		MaterielDAO materielDAO = new MaterielDAO(DAOFactory.getInstance());
 		
 		return materielDAO.get(idMateriel);
 	}
 	
-	public List<Materiel> recupererTousMateriels(){
+	public List<Materiel> recupererTousMateriels() {
 		List<Materiel> listeMateriel = new ArrayList<Materiel>();
 		MaterielDAO materielDAO = new MaterielDAO(DAOFactory.getInstance());
 		
@@ -79,47 +106,45 @@ public class MaterielCtrl {
 		return listeMateriel;
 	}
 	
-	public void modifierMateriel(Materiel materiel){
-		if(materiel.getEtat() == null){
+	public void modifierMateriel(Materiel materiel) {
+		if (materiel.getEtat() == null) {
 			throw new NullPointerException("L'etat ne doit pas etre null");
 		}
 		
-		if(materiel.getCategorie() == null){
+		if (materiel.getCategorie() == null) {
 			throw new NullPointerException("La categorie ne doit pas etre null");
 		}
 		
-		if(materiel.getMarque() == null){
+		if (materiel.getMarque() == null) {
 			throw new NullPointerException("la marque ne doit pas etre null");
 		}
 		
-		if(materiel.getDesignation() == null){
+		if (materiel.getDesignation() == null) {
 			throw new NullPointerException("La designation ne doit pas etre null");
 		}
 		
-		if(materiel.getTypeMat() == null){
+		if (materiel.getTypeMat() == null) {
 			throw new NullPointerException("le type de materiel ne doit pas etre null");
 		}
 		
-		if (materiel.getTypeMat() == ""){
+		if (materiel.getTypeMat() == "") {
 			throw new IllegalArgumentException("le type doit etre rempli");
 		}
 		
-		if(materiel.getDateAchat() == null){
+		if (materiel.getDateAchat() == null) {
 			throw new NullPointerException("La date d'achat ne doit pas etre null");
 		}
 		
-		if(materiel.getValeurAchat() < 0.0){
+		if (materiel.getValeurAchat() < 0.0) {
 			throw new IllegalArgumentException("la valeur d'achat ne peut pas etre negative");
 		}
 		
-		if(materiel.getValeurReap() < 0.0){
+		if (materiel.getValeurReap() < 0.0) {
 			throw new IllegalArgumentException("la valeur ne peut pas etre negative");
 		}
 		
 		MaterielDAO materielDAO = new MaterielDAO(DAOFactory.getInstance());
 		
 		materielDAO.update(materiel);
-		
-		
 	}
 }
