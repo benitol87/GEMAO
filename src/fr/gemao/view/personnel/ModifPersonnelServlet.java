e31fad8cd904a2a38d9e76e0b2b9d768b26018e4
Mr Corleone <corentinchargois@free.fr>
2015-01-16 15:22:03 +0100

    Corrections

--- src/fr/gemao/view/personnel/ModifPersonnelServlet.java
+++ src/fr/gemao/view/personnel/ModifPersonnelServlet.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpSession;
 
 import fr.gemao.ctrl.AjouterAdresseCtrl;
 import fr.gemao.ctrl.AjouterCommuneCtrl;
-import fr.gemao.ctrl.PersonneCtrl;
 import fr.gemao.ctrl.RecupererAdresseCtrl;
 import fr.gemao.ctrl.RecupererCommuneCtrl;
 import fr.gemao.ctrl.RecupererContratCtrl;
@@ -23,13 +22,12 @@ import fr.gemao.entity.Adresse;
 import fr.gemao.entity.Commune;
 import fr.gemao.entity.Contrat;
 import fr.gemao.entity.Diplome;
-import fr.gemao.entity.Personne;
 import fr.gemao.entity.Personnel;
 import fr.gemao.entity.Responsabilite;
 import fr.gemao.form.personnel.PersonnelForm;
 
 /**
- * Servlet implementation class ModifPersonnelServlet
+ * Servlet implémentant la classe ModifPersonnelServlet
  */
 @WebServlet("/personnel/ModifPersonnel")
 public class ModifPersonnelServlet extends HttpServlet {
@@ -39,51 +37,58 @@ public class ModifPersonnelServlet extends HttpServlet {
 	public static final String VUE_LISTE = "/personnel/ListePersonnel";
 
 	/**
-	 * Chargement de la page de modification. Le parametre idPersonne doit etre
-	 * envoyé pour le doGet (l'id correspond a celui de la personne à modifier.
+	 * Chargement de la page de modification d'un personnel.
+	 * Un paramètre doit être envoyé au doGet pour récupérer la personne concernée par les modifications.
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		HttpSession session = request.getSession();
-		String param = request.getParameter("idPersonne");
 		
+		
+		/* Si le paramètre id est null */
 		if(request.getParameter("id") == null){
 			this.getServletContext().getRequestDispatcher(VUE_LISTE).forward(request, response);
+			
+		/* Sinon */
 		} else {
 			long id = Long.parseLong(request.getParameter("id"));
 			RecupererPersonnelCtrl recupererPersonnelCtrl = new RecupererPersonnelCtrl();
 			Personnel personnel = recupererPersonnelCtrl.recupererPersonnel(id);
 
 			RecupererAdresseCtrl recupererAdresseCtrl = new RecupererAdresseCtrl();
-			Adresse adresse = recupererAdresseCtrl.recupererAdresse(personnel
-					.getAdresse().getIdAdresse());
+			Adresse adresse = recupererAdresseCtrl.recupererAdresse(personnel.getAdresse().getIdAdresse());
 
 			RecupererCommuneCtrl recupererCommuneCtrl = new RecupererCommuneCtrl();
-			Commune commune = recupererCommuneCtrl.recupererCommune(adresse
-					.getCommune().getIdCommune());
+			Commune commune = recupererCommuneCtrl.recupererCommune(adresse.getCommune().getIdCommune());
 
 			RecupererContratCtrl recupererContratCtrl = new RecupererContratCtrl();
-			Contrat contrat = recupererContratCtrl.recupererContrat(personnel
-					.getContrat().getIdContrat());
+			Contrat contrat = recupererContratCtrl.recupererContrat(personnel.getContrat().getIdContrat());
 
 			SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
 			String dateDebutContrat = formatter.format(contrat.getDateDebut());
 
 			String dateFinContrat = null;
+			
+			/* Si la date de fin du contrat n'est pas null */
 			if (contrat.getDateFin() != null) {
 				dateFinContrat = formatter.format(contrat.getDateFin());
 			}
 			
 			List<Responsabilite> listeResponsabilite = personnel.getListeResponsabilite();
+			
+			/* Si la liste des responsabilités est vide */
 			if(listeResponsabilite.isEmpty()){
 				listeResponsabilite.add(new Responsabilite(0, "Aucune"));
 			}
 			
 			List<Diplome> listeDiplome = personnel.getListeDiplomes();
+			
+			/* Si la liste des diplômes est vide */
 			if(listeDiplome.isEmpty()){
 				listeDiplome.add(new Diplome(0, "Aucun"));
 			}
 
+			/* Positionnement des attributs */
 			session.setAttribute("listeDiplome", listeDiplome);
 			session.setAttribute("listeResponsabilite", listeResponsabilite);
 			session.setAttribute("personnel", personnel);
@@ -94,8 +99,6 @@ public class ModifPersonnelServlet extends HttpServlet {
 			session.setAttribute("dateFinContrat", dateFinContrat);
 			this.getServletContext().getRequestDispatcher(VUE_MODIFICATION).forward(request, response);
 		}
-
-		
 	}
 
 	/**
@@ -104,11 +107,6 @@ public class ModifPersonnelServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
 		
-		long id = Long.parseLong(request.getParameter("id"));
-		
-		RecupererPersonnelCtrl recupererPersonneCtrl = new RecupererPersonnelCtrl();
-		Personne personne = recupererPersonneCtrl.recupererPersonnel(id);
-		
 		PersonnelForm form = new PersonnelForm();
 		
 		form.testerPersonnel(request);
@@ -116,19 +114,19 @@ public class ModifPersonnelServlet extends HttpServlet {
 		/* Récupération de la session depuis la requête */
 		HttpSession session = request.getSession();
 		
-		
-		
+		/* Si le formulaire n'a pas renvoyé d'erreur */
 		if (form.getErreurs().isEmpty()) {
-			PersonneCtrl persCtrl = new PersonneCtrl();
 			ModifierPersonnelCtrl modifPers = new ModifierPersonnelCtrl();
 			AjouterAdresseCtrl ajouterAdr = new AjouterAdresseCtrl();
 			AjouterCommuneCtrl ajouterCommune = new AjouterCommuneCtrl();
 			
 			Personnel pers = null;
 			
+			/* Si l'objet renvoyé est bien du type Personnel */
 			if (session.getAttribute("personnel").getClass() == Personnel.class) {
 				pers = (Personnel) session.getAttribute("personnel");
 				
+				/* Positionnement des attributs du pers à partir des informations du form */
 				pers.setTelFixe(form.getTelFixe());
 				pers.setTelPort(form.getTelPort());
 				pers.setEmail(form.getEmail());
@@ -136,6 +134,7 @@ public class ModifPersonnelServlet extends HttpServlet {
 				pers.setListeDiplomes(form.getListeDiplomes());
 				pers.setListeResponsabilite(form.getListeResponsabilite());
 				
+				/* Positionnement des attributs relatifs à la commune */
 				ajouterCommune.ajoutCommune(form.getAdresse().getCommune());
 				ajouterAdr.ajoutAdresse(form.getAdresse());
 				
@@ -147,7 +146,10 @@ public class ModifPersonnelServlet extends HttpServlet {
 			}
 		}
 		
+		/* Si le formulaire ne retourne pas d'erreur */
 		if (form.getErreurs().isEmpty()) {
+			
+			/* On redirige vers la liste des personnels */
 			response.sendRedirect(request.getContextPath() + VUE_LISTE);
 		} else {
 			this.getServletContext().getRequestDispatcher(VUE_MODIFICATION).forward(request, response);
