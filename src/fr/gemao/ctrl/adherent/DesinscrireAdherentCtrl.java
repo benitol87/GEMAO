0432e6cc643bbe2873905e7a78acf4ece8e7072b
CPoimboeuf <claire.poimboeuf@etu.unilim.fr>
2015-01-16 11:41:35 +0100

    Ajout de DesinscrireAdherentCtrl

---
+++ src/fr/gemao/ctrl/adherent/DesinscrireAdherentCtrl.java
@@ -0,0 +1,33 @@
+package fr.gemao.ctrl.adherent;
+
+import fr.gemao.entity.adherent.Adherent;
+import fr.gemao.sql.AdherentDAO;
+import fr.gemao.sql.DAOFactory;
+import fr.gemao.sql.PersonneDAO;
+
+public class DesinscrireAdherentCtrl {
+
+	public DesinscrireAdherentCtrl(){
+	}
+	
+	public void desinscrireAdherent(Adherent adherent){
+
+		//Vérification de l'existance de la personne
+		DAOFactory co = DAOFactory.getInstance();
+		PersonneDAO personneDAO = new PersonneDAO(co);
+		if (personneDAO.exist(adherent) != null) {
+			AdherentDAO adherentDAO = new AdherentDAO(co);
+			
+			//Mise à jour
+			Adherent adh = adherentDAO.update(adherent);		
+			if(adh == null) {
+				System.out.println("Une erreur est survenue lors de la désinscription.");
+			} else {
+				System.out.println("L'adhérent a bien été désinscrit.");
+			}
+		}
+		else{
+			System.out.println("L'adhérent n'existe pas dans la base...");
+		}
+	}
+}
