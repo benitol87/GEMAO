985349c1b2205e65575e88087feca62e5b0bf40d
clem19s <clemj1900@gmail.com>
2015-01-16 08:07:03 +0100

    Debut modifier AdherentServ

--- src/fr/gemao/view/adherent/ModifAdherentServlet.java
+++ src/fr/gemao/view/adherent/ModifAdherentServlet.java
@@ -7,6 +7,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.gemao.ctrl.PersonneCtrl;
+import fr.gemao.entity.Personnel;
 
 /**
  * Servlet implementation class ModifAdherentServlet
@@ -14,6 +18,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/adherent/ModifierAdherent")
 public class ModifAdherentServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
+	private static final String VUE_LISTE = "/GEMAO/adherent/listerAdherent";
 	
 	private String VUE = "/WEB-INF/pages/adherent/modifAdherent.jsp";
 
@@ -21,7 +27,19 @@ public class ModifAdherentServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+        HttpSession session = request.getSession();
+        String param = request.getParameter("id");
+        if (param != null) {
+			 int idParametre = Integer.parseInt(param);
+			 PersonneCtrl persCtrl = new PersonneCtrl();
+			 Personnel pers = persCtrl.recupererPersonnel(idParametre);
+			 
+			 session.setAttribute("sessionObjectPersonnel", pers);
+			 
+			 this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+		 } else {
+			 response.sendRedirect(VUE_LISTE);
+		 }
 	}
 
 	/**
