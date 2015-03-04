02ba1f677cc0b8b96470de822b291b0e7797937b
PaulMalet <paul.malet-plicque@etu.unilim.fr>
2015-03-04 19:25:37 +0100

    ajout designation/etat/fournisseur/etc... dans modifié est normalement fonctionnel

--- src/fr/gemao/view/materiel/ModifierMaterielServlet.java
+++ src/fr/gemao/view/materiel/ModifierMaterielServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -27,6 +28,14 @@ import fr.gemao.entity.materiel.Fournisseur;
 import fr.gemao.entity.materiel.Marque;
 import fr.gemao.entity.materiel.Materiel;
 import fr.gemao.form.materiel.MaterielForm;
+import fr.gemao.form.util.Form;
+import fr.gemao.sql.DAOFactory;
+import fr.gemao.sql.exception.DAOException;
+import fr.gemao.sql.materiel.CategorieDAO;
+import fr.gemao.sql.materiel.DesignationDAO;
+import fr.gemao.sql.materiel.EtatDAO;
+import fr.gemao.sql.materiel.FournisseurDAO;
+import fr.gemao.sql.materiel.MarqueDAO;
 import fr.gemao.view.JSPFile;
 import fr.gemao.view.Pattern;
 
@@ -34,6 +43,12 @@ import fr.gemao.view.Pattern;
 public class ModifierMaterielServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static String VUE_LISTE = "/materiel/ListerMateriel";
+	
+	private static String ERREUR_AJOUT_CATEGORIE = "erreurCat";
+	private static String ERREUR_AJOUT_DESIGNATION = "erreurDes";
+	private static String ERREUR_AJOUT_FOURNISSEUR = "erreurFour";
+	private static String ERREUR_AJOUT_MARQUE = "erreurMarque";
+	private static String ERREUR_AJOUT_ETAT = "erreurEtat";
 
 	/**
 	 * Chargement de la page de modification. Le parametre idMateriel doit etre
@@ -42,6 +57,26 @@ public class ModifierMaterielServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
+		List<Categorie> listCate = new ArrayList<Categorie>();
+		listCate = new CategorieDAO(DAOFactory.getInstance()).getAll();
+		request.setAttribute("LISTE_CATEGORIE", listCate);
+
+		List<Etat> listEtat = new ArrayList<Etat>();
+		listEtat = new EtatDAO(DAOFactory.getInstance()).getAll();
+		request.setAttribute("LISTE_ETAT", listEtat);
+
+		List<Designation> listDesi = new ArrayList<Designation>();
+		listDesi = new DesignationDAO(DAOFactory.getInstance()).getAll();
+		request.setAttribute("LISTE_DESIGNATION", listDesi);
+
+		List<Marque> listMarques = new ArrayList<Marque>();
+		listMarques = new MarqueDAO(DAOFactory.getInstance()).getAll();
+		request.setAttribute("LISTE_MARQUE", listMarques);
+
+		List<Fournisseur> listFourni = new ArrayList<Fournisseur>();
+		listFourni = new FournisseurDAO(DAOFactory.getInstance()).getAll();
+		request.setAttribute("LISTE_FOURNISSEUR", listFourni);
+		
 		try {
 			HttpSession session = request.getSession();
 
@@ -107,6 +142,79 @@ public class ModifierMaterielServlet extends HttpServlet {
 		/* Récupération de la session depuis la requête */
 		HttpSession session = request.getSession();
 		System.out.print(form.getErreurs());
+		
+		// Ajout d'une catégorie, le fait qu'elle ne soit pas vide
+				// a déjà été testé
+				if (Form.getValeurChamp(request, "nomCat") != null) {
+					try {
+						CategorieCtrl.ajoutCategorie(Form.getValeurChamp(request,
+								"nomCat"));
+					} catch (DAOException e) {
+						form.setErreur(ERREUR_AJOUT_CATEGORIE,
+								"La catégorie existe déjà");
+					}
+				}
+
+				if (Form.getValeurChamp(request, "nomDes") != null) {
+					try {
+						DesignationCtrl.ajoutDesignation(Form.getValeurChamp(request,
+								"nomDes"));
+					} catch (DAOException e) {
+						form.setErreur(ERREUR_AJOUT_DESIGNATION,
+								"La designation existe déjà");
+					}
+				}
+
+				if (Form.getValeurChamp(request, "nomFour") != null) {
+					try {
+						FournisseurCtrl.ajoutFournisseur(Form.getValeurChamp(request,
+								"nomFour"));
+					} catch (DAOException e) {
+						form.setErreur(ERREUR_AJOUT_FOURNISSEUR,
+								"Le Fournisseur existe déjà");
+					}
+				}
+				
+				if (Form.getValeurChamp(request, "nomEtat") != null) {
+					try {
+						EtatCtrl.ajoutEtat(Form.getValeurChamp(request,
+								"nomEtat"));
+					} catch (DAOException e) {
+						form.setErreur(ERREUR_AJOUT_ETAT,
+								"L'etat existe déjà");
+					}
+				}
+
+				if (Form.getValeurChamp(request, "nomMarque") != null) {
+					try {
+						MarqueCtrl.ajouterMarque(Form.getValeurChamp(request,
+								"nomMarque"));
+					} catch (DAOException e) {
+						form.setErreur(ERREUR_AJOUT_MARQUE,
+								"La marque existe déjà");
+					}
+				}
+
+				List<Categorie> listCat = new ArrayList<Categorie>();
+				listCat = new CategorieDAO(DAOFactory.getInstance()).getAll();
+				request.setAttribute("LISTE_CATEGORIE", listCat);
+
+				List<Etat> listEtat = new ArrayList<Etat>();
+				listEtat = new EtatDAO(DAOFactory.getInstance()).getAll();
+				request.setAttribute("LISTE_ETAT", listEtat);
+
+				List<Designation> listDes = new ArrayList<Designation>();
+				listDes = new DesignationDAO(DAOFactory.getInstance()).getAll();
+				request.setAttribute("LISTE_DESIGNATION", listDes);
+
+				List<Marque> listMarque = new ArrayList<Marque>();
+				listMarque = new MarqueDAO(DAOFactory.getInstance()).getAll();
+				request.setAttribute("LISTE_MARQUE", listMarque);
+
+				List<Fournisseur> listFourn = new ArrayList<Fournisseur>();
+				listFourn = new FournisseurDAO(DAOFactory.getInstance()).getAll();
+				request.setAttribute("LISTE_FOURNISSEUR", listFourn);
+				
 		if (form.getErreurs().isEmpty()) {
 			MaterielCtrl matctrl = new MaterielCtrl();
 			Materiel mat = null;
