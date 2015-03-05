1520dc182ad3526ec19d7ea105d1ba29a01f01ef
Masttest <r.gourier@hotmail.fr>
2015-03-05 13:46:27 +0100

    le changement des name (champ disciplines lors de la modification d'un adhérent marche)

--- WebContent/js/modifierAdherent.js
+++ WebContent/js/modifierAdherent.js
@@ -41,16 +41,26 @@ function ajoutNewDiscipline(){
 $(function() {
 	bouton1=document.getElementById("ajoutDiscipline");
 	bouton2=document.getElementById("retireDiscipline");
-	bouton3=document.getElementById("ajoutNewDiscipline");
+//	bouton3=document.getElementById("ajoutNewDiscipline");
 	ajouteEvent(bouton1, 'click', ajoutDiscipline, false);
 	ajouteEvent(bouton2, 'click', retireDiscipline, false);
-	ajouteEvent(bouton3, 'click', ajoutNewDiscipline, false);
+//	ajouteEvent(bouton3, 'click', ajoutNewDiscipline, false);
 	document.getElementById("select1").style.display="none";
 	
 	document.getElementById("disciplines1").setAttribute("disabled", "disabled");
 	
 	$(".supprimerDiscipline").click(function (e) {
 		var div = e.target.parentNode;
+		var listSuivant = [];
+		suivant = div.nextSibling.nextSibling;
+		while(suivant && suivant.id!="select1"){
+			listSuivant.push(suivant);
+			suivant = suivant.nextSibling.nextSibling;
+		}
+		for (i=listSuivant.length-1;i>=0;i--){
+			prev=listSuivant[i].previousSibling.previousSibling;
+			listSuivant[i].getElementsByTagName('input')[0].name=prev.getElementsByTagName('input')[0].name;
+		}			
 		div.parentNode.removeChild(div);
 	});
 });
